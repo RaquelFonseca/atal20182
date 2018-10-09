330b1d523a116700c4abde2980b8c1b9e9fcb9bc
Raquel Ambrozio <raquel.fonseca@ccc.ufcg.edu.br>
2018-10-09 11:35:35 -0300

    teste

--- atal/algorithms.py
+++ atal/algorithms.py
@@ -1,8 +1,14 @@
 # Esse metodo recebe uma lista com as matriculas dos alunos
 # e retorna essa lista em ordem crescente de matriculas
 def retorna_matriculas_decrescente(alist):
-	for j in xrange(len(alist)):
-		for i in xrange(len(alist)-1):
-			if alist[i] > alist[i+1]:
-				alist[i], alist[i+1] = alist[i+1], alist[i]
-	return alist
+            if alist:
+                    left = [x for x in alist if x > alist[0]]
+                    right = [x for x in alist if x < alist[0]]
+                    if len(left) > 1:
+                            left = retorna_matriculas_decrescente(left)
+                    if len(right) > 1:
+                             right = retorna_matriculas_decrescente(right)
+                    return left + [alist[0]] * alist.count(alist[0]) + right
+            return []
+
+print retorna_matriculas_decrescente([3, 8, 1])
